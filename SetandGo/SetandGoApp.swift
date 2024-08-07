1d85519a906972a1d9ffc0799f70e704f4dc6247
dpolygon <dannyg@lonestardev.net>
2024-08-07 16:21:38 -0500

    Initial Commit

---
+++ SetandGo/SetandGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetandGoApp.swift
+//  SetandGo
+//
+//  Created by Daniel Gonzalez on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SetandGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
